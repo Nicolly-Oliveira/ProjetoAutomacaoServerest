fa2ca3db23ffb4be608ef42781f9d8468db6c2c4
isabellemr <isabelle-ramalho@outlook.com>
2025-05-16 10:13:58 -0300

    Pronto

--- src/test/java/dto/CarrinhoDTO.java
+++ src/test/java/dto/CarrinhoDTO.java
@@ -4,15 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarrinhoDTO {
+
+    /**
+     * Lista que armazena todos os itens do carrinho
+     * Cada item é representado por um ItemCarrinhoDTO
+     */
     private List<ItemCarrinhoDTO> produtos;
 
+    /**
+     * Construtor que inicializa um novo carrinho com um único produto.
+     * @param idProduto identificador único do produto
+     * @param quantidade número de unidades do produto
+     */
     public CarrinhoDTO(String idProduto, int quantidade) {
         this.produtos = new ArrayList<>();
         this.produtos.add(new ItemCarrinhoDTO(idProduto, quantidade));
     }
 
+    /**
+     * Retorna a lista de todos os produtos no carrinho.
+     * @return Lista contendo todos os itens do carrinho
+     */
     public List<ItemCarrinhoDTO> getProdutos() {
         return produtos;
     }
+
 }
 
